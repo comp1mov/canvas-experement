911cd5e316aa5cfdeca08cddfda6e9a2af60de98
Grisha Tsvetkov <119138897+comp1mov@users.noreply.github.com>
2025-11-15 01:37:54 +0200

    Update pFX_for_CV_page.js

--- pFX_for_CV_page.js
+++ pFX_for_CV_page.js
@@ -64,18 +64,18 @@
     // --- влияние указателя в базовом режиме ---
     pointerInfluenceRadius: 500,   // px: радиус влияния указателя, умножается на dpr
     enablePointerSwirl: true,      // bool: тангенциальный вихрь
-    pointerSwirlStrength: 2.0,     // коэф: сила вихря
-    pointerSwirlFalloffExp: 1.4,   // коэф: затухание по радиусу, 1..3
+    pointerSwirlStrength: 1.0,     // коэф: сила вихря
+    pointerSwirlFalloffExp: 2.4,   // коэф: затухание по радиусу, 1..3
     pointerAttractionStrength: 0.1,// коэф: сила притяжения (0.1-2.0)
     enablePointerAttraction: true, // bool: притяжение к указателю ← ДОБАВЬ ЭТУ СТРОКУ
     enablePointerNoise: true,      // bool: синус нойз-вектор от указателя
-    pointerNoiseAmp: 2.0,          // коэф: амплитуда нойза
-    pointerNoiseHz: 0.3,           // Гц: частота нойза
+    pointerNoiseAmp: 5.0,          // коэф: амплитуда нойза
+    pointerNoiseHz: 0.1,           // Гц: частота нойза
     pointerNoiseSmooth: 0.95,      // 0..0.999: сглаживание нойз-вектора
 
     // --- клик: pull -> burst ---
     clickAffectsAll: false,        // bool: true трогать все точки, false только в радиусе
-    clickRadius: 350,              // px: радиус набора точек для клика, умножается на dpr
+    clickRadius: 150,              // px: радиус набора точек для клика, умножается на dpr
     prePullSec: 0.6,               // сек: длительность стягивания к центру
     pullGrowFactor: 0.0,           // коэф: масштаб точки в pull (1.0 без изменений)
     burstLife: 3.8,                // сек: длительность разлёта
